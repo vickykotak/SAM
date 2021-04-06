396b631f2732d7b873776e2adeeb198f39bb9877
vickykotak <vickykotak09@gmail.com>
2021-04-06 14:39:14 +0530

    cidr added

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const Shell = require('node-powershell');
 const netList = require('network-list');
+const IPCIDR = require("ip-cidr");
+const ip = require("ip");
 
 const app = express()
 const port = 5000
-
 
 const ps = new Shell({
     executionPolicy: 'Bypass',
     noProfile: true
 });
 
-// manually add ip for time being
-const command = `wmic /node:192.168.97.132 product get name,identifyingnumber,installdate,vendor,version /format:htable`;
-const c= "ping 192.168.97.243";
 
-app.get('/', (req, res) => {
-    ps.addCommand(`cd ~`);
-    ps.addCommand(`cd desktop`);
+// get list of softwares. 
+// It will take few minutes to get response
+app.get('/softwares', (req, res) => {
+    const command = `wmic /node:${ip.address()} product get name,identifyingnumber,installdate,vendor,version /format:htable`;
+    // ps.addCommand(`cd ~`);
+    // ps.addCommand(`cd desktop`);
     ps.addCommand(command);
     // ps.addCommand(`$pwd.Path`);
     ps.invoke()
@@ -29,14 +30,44 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/sm', (req, res) => {
-    netList.scan({
-        ip: '192.168.97', // add manually
-        timeout: 10000, 
-        vendor: true
-    }, (err, arr) => {
-        res.send(arr); // array with all devices
+// get active devices
+app.get('/active', (req, res) => {
+    let active = [];
+
+    netList.scan({}, (err, arr) => {
+        active = arr.filter(arr => (arr.alive) )
+        res.send(active); // array with all active devices
     });
+});
+
+// get inactive devices
+app.get('/inactive', (req, res) => {
+    let active = [];
+
+    netList.scan({}, (err, arr) => {
+        active = arr.filter(arr => !(arr.alive))
+        res.send(active); // array with all inactive devices
+    });
+});
+
+// get list of ip over wide range
+app.get('/cidr', (req, res) => {
+    let ips= [];
+    let IP = ip.address();
+    // change cidr notaion to get wide range of ip's ( IP/16 => 65563 ip's )
+    const cidr = new IPCIDR(IP+'/23');
+
+    if (!cidr.isValid()) {
+        throw new Error('CIDR is invalid');
+    }
+
+    cidr.loop(ip => { ips.push(ip) });
+    console.log(ips.length); // total ips detected
+    res.send(ips);
+});
+
+app.get('/', (req, res) => {
+   
 });
  
 
